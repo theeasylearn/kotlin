f580f769c2b254101ac2dcb358b018e7aba1574e
The EasyLearn Academy <theeasylearn@users.noreply.github.com>
2021-07-14 19:33:27 +0530

    Create extension_function2.kt

---
+++ extension_function2.kt
@@ -0,0 +1,17 @@
+class Circle(var radius:Float)
+{
+    fun getArea() : Double
+    {
+        return Math.PI * radius * radius
+    }
+}
+fun Circle.getPerimeter() :Double
+{
+    return 2 * Math.PI * radius        
+}
+fun main()
+{
+    var c1 = Circle(100.0f);
+    println("area of circle is " + c1.getArea())
+    println("permimeter of circle is " + c1.getPerimeter())
+}
